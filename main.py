f6eafe585e6076a3d070be587402d4af8cd63bd3
Laura C. Balbachan dos Santos <balbachanlaura@gmail.com>
2024-11-06 08:01:17 -0300

    Adicionar a dona Thelma

--- main.py
+++ main.py
@@ -42,11 +42,42 @@ class Car():
     def draw(self):
         screen.blit(self.image, self.rect)
 
+# Classe do Player
+class Player():
+    def __init__(self):
+        self.image = pg.image.load("./assets/icons/skater/skater_0.png").convert()
+        self.image.set_colorkey((255, 255, 255)) # Faz o png ficar transparente
+        self.image = pg.transform.scale_by(self.image, 0.1) # Faz o scale proporcional da imagem
+        self.position = pg.math.Vector2(screen_width // 2, screen_height - 100)
+        self.speed = 5
+        self.rect = self.image.get_rect(center=self.position)
+
+    def handle_keys(self):
+        keys = pg.key.get_pressed()
+        if keys[K_LEFT]:
+            self.position.x -= self.speed
+        if keys[K_RIGHT]:
+            self.position.x += self.speed
+        if keys[K_UP]:
+            self.position.y -= self.speed
+        if keys[K_DOWN]:
+            self.position.y += self.speed
+
+        # Limita o movimento do jogador dentro da tela
+        self.position.x = max(0, min(screen_width - self.rect.width, self.position.x))
+        self.position.y = max(0, min(screen_height - self.rect.height, self.position.y))
+        
+        self.rect.center = self.position
+
+    def draw(self):
+        screen.blit(self.image, self.rect)
+
 def main():
     clock = pg.time.Clock()
     running = True
 
     car = Car()
+    player = Player()
 
     # Carregar a imagem de fundo
     background = pg.image.load("./assets/bgs/bg#0.jpg").convert()
@@ -70,8 +101,13 @@ def main():
         screen.blit(background, (0, background_y))
         screen.blit(background, (0, background_y - screen_height))
 
+        # Atualiza e desenha o carro
         car.update()
         car.draw()
+        
+        # Atualiza e desenha o jogador
+        player.handle_keys()
+        player.draw()
         
         # Atualiza a tela
         pg.display.flip()
